37e8ebe1de5323d624d0d6092cad6c9e781b11a4
Jinu Lee <jinulee.v@gmail.com>
2023-02-08 12:02:00 +0000

    Added appropriate eval script

--- eval.sh
+++ eval.sh
@@ -3,13 +3,13 @@ export TOKENIZERS_PARALLELISM=false
 # python inference.py \
 #     --test_data data/qqp_paragen_test.json \
 #     --model_postfix bart_qqp_1gen
-# python inference.py \
-#     --test_data data/qqp_paragen_test.json \
-#     --model_postfix bart_qqp_1gen_2gen+triecl
-# python inference.py \
-#     --test_data data/qqp_paragen_test.json \
-#     --model_postfix bart_qqp_1gen+triecl
+python inference.py \
+    --test_data data/qqp_paragen_test.json \
+    --model_postfix bart_qqp_1gen_2gen+brio
+python inference.py \
+    --test_data data/qqp_paragen_test.json \
+    --model_postfix bart_qqp_1gen+brio
 
-python eval.py --model_postfix bart_qqp_1gen
-python eval.py --model_postfix bart_qqp_1gen_2gen+triecl
-python eval.py --model_postfix bart_qqp_1gen+triecl
+# python eval.py --model_postfix bart_qqp_1gen
+python eval.py --model_postfix bart_qqp_1gen_2gen+brio
+python eval.py --model_postfix bart_qqp_1gen+brio
