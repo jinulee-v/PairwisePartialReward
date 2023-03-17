4d9e84ead5a98fc416d93f3ed9741e79d63c4e0e
Jinu Lee <jinulee.v@gmail.com>
2023-03-17 01:48:09 +0000

    Implemented NMT dataset downloader via huggingface & miscellaneous

---
+++ dataset_downloader_nmt.py
@@ -0,0 +1,22 @@
+from datasets import *
+import json
+
+for set, subset in [("wmt14", "de-en"), ("wmt15", "fr-en"), ("wmt16", "ro-en")]:
+    print(set, subset)
+    ds_builder = load_dataset_builder(set, subset)
+    second_lang = subset[0:2]
+    cnt = 0
+    for split in ["train", "validation", "test"]:
+        dataset = load_dataset(set, subset, split=split)
+        new_dataset = []
+        for datum in dataset:
+            new_dataset.append({
+                "id": cnt,
+                "source": datum["translation"]["en"],
+                "targets": [datum["translation"][second_lang]],
+            })
+            cnt += 1
+
+        split = "dev" if split == "validation" else split
+        with open(f"data/{set}_en-{second_lang}_translation_{split}.json", "w", encoding="UTF-8") as file:
+            json.dump(new_dataset, file, ensure_ascii=False, indent=4)
--- setup.sh
+++ setup.sh
@@ -1,3 +1,5 @@
 pip install -r requirements.txt
 
+python dataset_downloader_nmt.py # Download NMT dataset via huggingface
+
 python hotfix_diaparser.py # Hotfix diaparser module for compatibility with torch==1.13.1
