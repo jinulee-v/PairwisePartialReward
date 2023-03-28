6aa1381e760ebca175a983ef5d497125c95b1fad
Jinu Lee <jinulee.v@gmail.com>
2023-03-28 08:24:30 +0000

    Modified setup scripts

--- setup.sh
+++ setup.sh
@@ -1,5 +1,16 @@
 pip install -r requirements.txt
 
-python dataset_downloader_nmt.py # Download NMT dataset via huggingface
+# Download NMT dataset via huggingface
+python dataset_downloader_nmt.py
 
-python hotfix_diaparser.py # Hotfix diaparser module for compatibility with torch==1.13.1
+# Hotfix diaparser module for compatibility with torch==1.13.1
+python hotfix_diaparser.py 
+
+# Download comet small model
+wget https://unbabel-experimental-models.s3.amazonaws.com/comet/eamt22/eamt22-cometinho-da.tar.gz
+tar -xf eamt22-cometinho-da.tar.gz
+# Move to cache directory
+mkdir ~/.comet
+mv eamt22-cometinho-da ~/.comet/eamt22-cometinho-da
+# Remove tarball
+rm eamt22-cometinho-da.tar.gz
